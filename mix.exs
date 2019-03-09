3c5c43b6937501ea9599d33df41d8beb038bc18d
Mingyar Furtado <mingyar.furtado@gmail.com>
2019-03-09 04:02:15 -0300

    mingyar-patch-fix-postgrex-ver

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Discuss.Mixfile do
       {:phoenix, "~> 1.3.4"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
-      {:postgrex, ">= 0.0.0"},
+      {:postgrex, "~> 0.14.1"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
