99cd0233462f3c3a12b19f295d37c6bd6fa915f4
Mingyar Furtado <mingyar.furtado@gmail.com>
2019-03-09 01:55:45 -0300

    Fix config tests

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,9 @@ config :logger, level: :warn
 # Configure your database
 config :discuss, Discuss.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("POSTGRES_USER") || "postgres",
+  password: System.get_env("POSTGRES_PASSWORD") || "postgres",
   database: "discuss_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
+  
